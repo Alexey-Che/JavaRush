ce9077705bac7bcf16a84ae88f74e799f7687237
Alexey Korotkih <lexa.36484@gmail.com>
2021-03-03 20:24:10 +0500

    comment

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Integer a = 1000;
+        print(a);
+        print(1);
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print(Integer integer) {
+
+    }
+    public static void print(int x) {
+
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print() {
+    }
+
+    public static void print(Integer x) {
+    }
+
+    public static void print(String s) {
+    }
+    public static void print(double d) {
+    }
+    public static void print(long l) {
+    }
+}
