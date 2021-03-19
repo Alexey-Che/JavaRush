4892ecc841948af3a50d95ba738edd260c73d79d
Alexey Korotkih <lexa.36484@gmail.com>
2021-03-19 11:23:23 +0500

    comment

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1911/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task19.task1911;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/* 
+Ридер обертка
+*/
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream console = System.out;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(byteArrayOutputStream);
+        System.setOut(stream);
+        testString.printSomething();
+        System.setOut(console);
+
+        String result = byteArrayOutputStream.toString().toUpperCase();
+        System.out.println(result);
+
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
