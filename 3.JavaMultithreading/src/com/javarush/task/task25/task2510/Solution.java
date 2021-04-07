abfe136c99aa766ee00a3fd40ebd0074f0bc9ad9
Alexey Korotkih <lexa.36484@gmail.com>
2021-04-07 11:50:06 +0500

    comment

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2510/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task25.task2510;
+
+/* 
+Поживем - увидим
+*/
+
+public class Solution extends Thread {
+
+    public Solution() {
+        this.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                String message = null;
+                if (e instanceof Error) message = "Нельзя дальше работать";
+                else if (e instanceof Exception) message = "Надо обработать";
+                else if (e instanceof Throwable) message = "Поживем - увидим";
+                System.out.println(message);
+            }
+        });
+
+    }
+
+    public static void main(String[] args) {
+    }
+}
