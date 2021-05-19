aa4ea85a871d82fca14b4c6583d1530c21e780fb
Alexey Korotkih <lexa.36484@gmail.com>
2021-05-19 09:55:13 +0500

    comment

---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2913/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task29.task2913;
+
+import java.util.Random;
+
+/* 
+Замена рекурсии
+*/
+
+public class Solution {
+    private static int numberA;
+    private static int numberB;
+
+    public static String getAllNumbersBetween(int a, int b) {
+        StringBuilder str = new StringBuilder(String.valueOf(a));
+        while (a != b) {
+            str.append(a < b ? " " + ++a : " " + --a);
+        }
+        return str.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        numberA = random.nextInt(1000);
+        numberB = random.nextInt(1000);
+        System.out.println(getAllNumbersBetween(numberA, numberB));
+        System.out.println(getAllNumbersBetween(numberB, numberA));
+    }
+}
