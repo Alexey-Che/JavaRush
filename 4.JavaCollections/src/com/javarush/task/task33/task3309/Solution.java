5968490b00599174f4cb794d8d0d7e7dbff27caa
Alexey Korotkih <lexa.36484@gmail.com>
2021-06-13 16:35:59 +0500

    comment

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3309/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task33.task3309;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.ByteArrayInputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Комментарий внутри xml
+*/
+
+public class Solution {
+    public static String toXmlWithComment(Object obj, String tagName, String comment) throws JAXBException {
+        JAXBContext context = JAXBContext.newInstance(obj.getClass());
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        StringWriter writer = new StringWriter();
+        marshaller.marshal(obj, writer);
+        String xml = writer.toString();
+        String result = null;
+        if (xml.indexOf(tagName) > -1)
+            result = xml.replaceAll("<" + tagName + ">", "<!--" + comment + "-->\n" + "<" + tagName + ">");
+        else
+            result = xml;
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
