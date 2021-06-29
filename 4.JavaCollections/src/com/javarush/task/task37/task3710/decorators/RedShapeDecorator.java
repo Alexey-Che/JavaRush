2f2bfe727a5b6abc393be55d9b35b8425a871ec6
Алексей Коротких <lexa.36484@gmail.com>
2021-06-29 10:19:48 +0500

    comment

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3710/decorators/RedShapeDecorator.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task37.task3710.decorators;
+
+import com.javarush.task.task37.task3710.shapes.Shape;
+
+public class RedShapeDecorator extends ShapeDecorator {
+
+    private Shape shape;
+
+    public RedShapeDecorator(Shape decoratedShape) {
+        super(decoratedShape);
+        this.shape = decoratedShape;
+    }
+
+    @Override
+    public void draw() {
+        setBorderColor(shape);
+        super.draw();
+    }
+
+    private void setBorderColor(Shape shape) {
+        System.out.println("Setting the border color for " + shape.getClass().getSimpleName() + " to red.");
+
+    }
+}
