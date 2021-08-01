31ade40650ac3a9039997abbc1a864b9d24d5e10
Алексей Коротких <lexa.36484@gmail.com>
2021-08-01 09:34:30 +0500

    comment

---
+++ 4.JavaCollections/src/com/javarush/task/task40/Test.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task40;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+import javax.mail.Session;
+import java.util.Properties;
+
+public class Test {
+
+    Properties props = new Properties();
+
+//тут надо загрузить данные в объект props
+
+    Session session = Session.getDefaultInstance(props);
+
+
+}
